407342d3deb48871ee4d3ae762ee2df94fb096ab
dmt-mynarski <dmt.sebastian@lo5.bielsko.pl>
2023-03-29 20:31:15 +0200

    Added new test for win flag

--- TicTacToe/TicTacToeTest.java
+++ TicTacToe/TicTacToeTest.java
@@ -1,10 +1,13 @@
 package TicTacToe;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TicTacToeTest {
+
     @DisplayName("Should returns board fiels from string")
     @Test
     void testConvertStrToField() {
@@ -14,5 +17,31 @@ public class TicTacToeTest {
         int[] strMarks = TicTacToe.convertStrToField("02");
         // then
         assertArrayEquals(marks, strMarks);
+    }
+
+    @DisplayName("Should returns true when there are three marks in line on board")
+    @Test
+    void testIfWin() {
+        // given
+        TicTacToe ttt = new TicTacToe();
+        char[][] marks1 = {{'X', 'X', 'X'}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        // when
+        TicTacToe.setBoard(marks1);
+        // then
+        assertTrue(ttt.ifWin());
+
+        // given
+        char[][] marks2 = {{'X', ' ', ' '}, {'X', ' ', ' '}, {'X', ' ', ' '}};
+        // when
+        TicTacToe.setBoard(marks2);
+        // then
+        assertTrue(ttt.ifWin());
+
+        // given
+        char[][] marks3 = {{'X', ' ', ' '}, {' ', 'X', ' '}, {' ', ' ', 'X'}};
+        // when
+        TicTacToe.setBoard(marks3);
+        // then
+        assertTrue(ttt.ifWin());
     }
 }
