bd2faf717c8497d63e4359ee6c975445956f0443
dmt-mynarski <dmt.sebastian@lo5.bielsko.pl>
2023-03-30 19:05:15 +0200

    Added verify win feature

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -6,84 +6,139 @@ public class TicTacToe {
 
     private static Scanner input = new Scanner(System.in);
     private static char[][] board = new char[3][3];
+    private static int[] lastMark = new int[2];
 
-public static void main(String[] args) {
-    TicTacToe ttt = new TicTacToe();
-    fillArray();
-    ttt.printBoard();
-    ttt.executeGame();
-    
-}
+    public TicTacToe() {
+    }
 
-private static void fillArray() {
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            board[i][j] = ' ';
+    public static void main(String[] args) {
+        TicTacToe ttt = new TicTacToe();
+        fillArray();
+        ttt.printBoard();
+        ttt.executeGame();
+
+    }
+
+    public static char[][] getBoard() {
+        return board;
+    }
+
+    public static void setBoard(char[][] board) {
+        TicTacToe.board = board;
+    }
+
+    public static int[] getLastMark() {
+        return lastMark;
+    }
+
+    public static void setLastMark(int[] lastMark) {
+        TicTacToe.lastMark = lastMark;
+    }
+
+
+    private static void fillArray() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
         }
     }
-}
 
-public void printBoard() {
-    String underRowOfBoard = "\n      [0] [1] [2]\n";
-    String firstRowOfBoard = "\n[0]  | " + board[0][0] + " | "  + board[0][1] + " | " + board[0][2] + " |\n";
-    String betweenRowOfBoard = "     ----+---+----\n";
-    String secondRowOfBoard = "[1]  | " + board[1][0] + " | "  + board[1][1] + " | " + board[1][2] + " |\n";
-    String thirdRowOfBoard = "[2]  | " + board[2][0] + " | "  + board[2][1] + " | " + board[2][2] + " |\n";
-    String strBoard = underRowOfBoard + firstRowOfBoard + betweenRowOfBoard + secondRowOfBoard + betweenRowOfBoard + thirdRowOfBoard;             
-    System.out.println(strBoard);
-}
-
-public void markX(String marks) {
-    int[] field = convertStrToField(marks);
-    board[field[0]][field[1]] = 'X';
-}
-
-public void markO(String marks) {
-    int[] field = convertStrToField(marks);
-    board[field[0]][field[1]] = 'O';
-}
-
-public static int[] convertStrToField(String mark) {
-    final int CHAR_ZERO_VALUE = 48;
-    int x = mark.charAt(0) - CHAR_ZERO_VALUE;
-    int y = mark.charAt(1) - CHAR_ZERO_VALUE;
-    return new int[] {x, y};
-}
-
-public void enterFieldName(int noPlayer) {
-    System.out.print("Podaj pole, które chcesz zaznaczyć (np. [0][0] wpisz 00): ");
-    String userMark = input.next();
-    if (noPlayer == 0) 
-        markX(userMark);
-    else
-        markO(userMark);
-}
-
-public void switchPlayer(int noPlayer) {
-    switch (noPlayer) {
-        case 0:
-            System.out.println("Gracz 1 -> zaznacz 'X'");
-            enterFieldName(0);
-            printBoard();
-            break;
-    
-        case 1:
-            System.out.println("Gracz 2 -> zaznacz 'O'");
-            enterFieldName(1);
-            printBoard();
-            break;
+    public void printBoard() {
+        String underRowOfBoard = "\n      [0] [1] [2]\n";
+        String firstRowOfBoard = "\n[0]  | " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |\n";
+        String betweenRowOfBoard = "     ----+---+----\n";
+        String secondRowOfBoard = "[1]  | " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |\n";
+        String thirdRowOfBoard = "[2]  | " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |\n";
+        String strBoard = underRowOfBoard + firstRowOfBoard + betweenRowOfBoard + secondRowOfBoard + betweenRowOfBoard
+                + thirdRowOfBoard;
+        System.out.println(strBoard);
     }
-}
 
-public void executeGame() {
-    int noPlayer = 0;
-    final int MAX_MOVES = 9;
-    int moves = 0;
-    while (moves < MAX_MOVES) {
-        switchPlayer(noPlayer);
-        noPlayer = ++noPlayer % 2;
-        moves++;
+    public void markX(String marks) {
+        int[] field = convertStrToField(marks);
+        board[field[0]][field[1]] = 'X';
+        lastMark[0] = field[0];
+        lastMark[1] = field[1];
     }
-}
+
+    public void markO(String marks) {
+        int[] field = convertStrToField(marks);
+        board[field[0]][field[1]] = 'O';
+        lastMark[0] = field[0];
+        lastMark[1] = field[1];
+    }
+
+    public static int[] convertStrToField(String mark) {
+        final int CHAR_ZERO_VALUE = 48;
+        int x = mark.charAt(0) - CHAR_ZERO_VALUE;
+        int y = mark.charAt(1) - CHAR_ZERO_VALUE;
+        return new int[] { x, y };
+    }
+
+    public void enterFieldName(int noPlayer) {
+        System.out.print("Podaj pole, które chcesz zaznaczyć (np. [0][0] wpisz 00): ");
+        String userMark = input.next();
+        if (noPlayer == 0)
+            markX(userMark);
+        else
+            markO(userMark);
+    }
+
+    public void switchPlayer(int noPlayer) {
+        switch (noPlayer) {
+            case 0:
+                System.out.println("Gracz 1 -> zaznacz 'X'");
+                enterFieldName(0);
+                printBoard();
+                if(isWin(noPlayer)) {
+                    System.out.println("Gracz 1 WYGRYWA!");
+                    System.exit(0);
+                }
+                break;
+
+            case 1:
+                System.out.println("Gracz 2 -> zaznacz 'O'");
+                enterFieldName(1);
+                printBoard();
+                if(isWin(noPlayer)) {
+                    System.out.println("Gracz 1 WYGRYWA!");
+                    System.exit(0);
+                }
+                break;
+        }
+    }
+
+    public void executeGame() {
+        int noPlayer = 0;
+        final int MAX_MOVES = 9;
+        int moves = 0;
+        while (moves < MAX_MOVES) {
+            switchPlayer(noPlayer);
+            noPlayer = ++noPlayer % 2;
+            moves++;
+        }
+    }
+
+    public boolean isWin(int noPlayer) {
+        boolean winFlag = false;
+        char playerMark;
+
+        if(noPlayer == 0)
+            playerMark = 'X';
+        else
+            playerMark = 'O';
+
+        if (board[lastMark[0]][0] == playerMark & board[lastMark[0]][1] == playerMark & board[lastMark[0]][2] == playerMark) {
+            winFlag = true;
+        } else if (board[0][lastMark[1]] == playerMark & board[1][lastMark[1]] == playerMark & board[2][lastMark[1]] == playerMark) {
+            winFlag = true;
+        } else if (board[0][0] == playerMark & board[1][1] == playerMark & board[2][2] == playerMark) {
+            winFlag = true;
+        } else if (board[0][2] == playerMark & board[1][1] == playerMark & board[2][0] == playerMark) {
+            winFlag = true;
+        }
+        return winFlag;
+    }
 
 }
